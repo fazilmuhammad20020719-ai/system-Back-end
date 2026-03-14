4d588c8b088ef4d8e512047dee5b189c28aae668
Fazil muhammad <fazilmuhammad20020719@gmail.com>
2026-03-15 00:58:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -82,7 +82,8 @@ const port = process.env.PORT || 5000;
 
 // --- MIDDLEWARE ---
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Track current request URL for error labeling
 app.use((req, _res, next) => { process._currentRoute = `${req.method} ${req.path}`; next(); });
